5b240dadf78f3fa737453656c6f09a81490f98cf
samuno717 <frymax717@gmail.com>
2026-05-20 13:03:56 +0200

    Config and .gitignore changes

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-import os
+from pathlib import Path
 
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
-NOTEBOOKS_DIR = os.path.join(PROJECT_ROOT, 'notebooks')
-SRC_DIR = os.path.join(PROJECT_ROOT, 'src')
+PROJECT_ROOT = Path(__file__).resolve().parent
+DATA_DIR = PROJECT_ROOT / 'data'
+NOTEBOOKS_DIR = PROJECT_ROOT / 'notebooks'
+SRC_DIR = PROJECT_ROOT / 'src'
