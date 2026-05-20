768dcaf1ba8411604b7cac04d3b1fd5196092a4e
samuno717 <frymax717@gmail.com>
2026-05-20 12:59:02 +0200

    Structure reorganization, env setup

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATA_DIR = os.path.join(PROJECT_ROOT, 'data')
+NOTEBOOKS_DIR = os.path.join(PROJECT_ROOT, 'notebooks')
+SRC_DIR = os.path.join(PROJECT_ROOT, 'src')
