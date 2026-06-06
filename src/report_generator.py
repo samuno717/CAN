a22756ad369344c832128dd474161f687307d1d9
samuno717 <frymax717@gmail.com>
2026-06-06 11:32:46 +0200

    Added script to generate data report

---
+++ src/report_generator.py
@@ -0,0 +1,22 @@
+from config import DATA_DIR
+import pandas as pd
+
+import sys
+import pip._vendor.pkg_resources as pkg_resources
+sys.modules['pkg_resources'] = pkg_resources
+
+from data_profiling import ProfileReport
+import os
+
+
+datafile = DATA_DIR / 'processed_data.csv'
+
+if datafile not in os.listdir(DATA_DIR):
+    sys.exit("ERROR: Processed data file not present in data folder! Run src.main.py")
+
+df = pd.read_csv()
+
+profile = ProfileReport(df, title="Raport CAN")
+profile.to_file(DATA_DIR / "report.html")
+
+print("Done! Report saved in data folder.")
