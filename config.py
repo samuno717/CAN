93a503760272892d1fc4ecb7d4f4c7df016e28f6
samuno717 <frymax717@gmail.com>
2026-06-11 15:06:52 +0200

    Added charts dir path

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ PROJECT_ROOT = Path(__file__).resolve().parent
 DATA_DIR = PROJECT_ROOT / 'data'
 NOTEBOOKS_DIR = PROJECT_ROOT / 'notebooks'
 SRC_DIR = PROJECT_ROOT / 'src'
+CHARTS_DIR = PROJECT_ROOT / 'charts'
